48a433ed582795f09a8a6b9dc352a5cb12f86e0f
Harmeet Matharoo <hmjatt@gmail.com>
2024-09-18 06:25:35 -0400

    Update Record (DTO/Entity) Class

--- src/Record.ts
+++ src/Record.ts
@@ -1,9 +1,21 @@
 export class Record {
     constructor(
-        public id: number,
-        public name: string,
-        public age: number,
-        public jobTitle: string,
-        public department: string,
+        public date: string,
+        public month: number,
+        public year: number,
+        public company: string,
+        public pipeline: string,
+        public keyPoint: string,
+        public latitude: number,
+        public longitude: number,
+        public directionOfFlow: string,
+        public tradeType: string,
+        public product: string,
+        public throughput: number,
+        public committedVolumes: number,
+        public uncommittedVolumes: number,
+        public nameplateCapacity: number,
+        public availableCapacity: number,
+        public reasonForVariance: string
     ) { }
 }
