e90808f979dd62a83430a8a89d689a58704e8e82
Harmeet Matharoo <hmjatt@gmail.com>
2024-09-18 06:11:11 -0400

    Set Up File-IO and Record Object (Entity/DTO)

---
+++ src/Record.ts
@@ -0,0 +1,9 @@
+export class Record {
+    constructor(
+        public id: number,
+        public name: string,
+        public age: number,
+        public jobTitle: string,
+        public department: string,
+    ) { }
+}
